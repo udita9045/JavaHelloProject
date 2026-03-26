6c7cbe5ac143645460af3a3f729806d62cc4e3b5
Udita <uditahalder2007@gmail.com>
2026-03-26 19:33:50 +0530

    UC7: Greeting using String.join()

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,14 +6,7 @@ public class HelloWorld {
             return;
         }
 
-        String result = "";
-
-        for (String name : args) {
-            result += name + ", ";
-        }
-
-        // Remove last comma and space
-        result = result.substring(0, result.length() - 2);
+        String result = String.join(", ", args);
 
         System.out.println("Hello, " + result + "!");
     }
