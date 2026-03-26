cc31be19c12be9084fb5da2adec53e7ca6c97e36
Udita <uditahalder2007@gmail.com>
2026-03-26 19:24:07 +0530

    UC3: Display Hello with default name handling

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
+        String name = "World";  // default value
+
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello World");
+            name = args[0];
         }
 
+        System.out.println("Hello " + name);
     }
 }
