2fd66d3b28239629d60bbef103070bf29a29587e
Udita <uditahalder2007@gmail.com>
2026-03-26 19:28:38 +0530

    UC5: Enhanced for loop with formatted greeting

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,12 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Hello World");
-        } else {
-            for (int i = 0; i < args.length; i++) {
-                System.out.println("Hello " + args[i]);
-            }
+            System.out.println("Hello, World!");
+            return;
         }
 
+        for (String name : args) {
+            System.out.println("Hello, " + name + "!");
+        }
     }
 }
